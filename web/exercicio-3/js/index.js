56038d9386249642ae1059f2d7743c5fe37ac02d
paulogrillo <paulodigitalart@gmail.com>
2021-10-22 15:20:59 -0300

    Updated exercise 3

--- web/exercicio-3/js/index.js
+++ web/exercicio-3/js/index.js
@@ -1,13 +1,52 @@
+let fname = window.document.getElementById('fname')
+let lname = document.querySelector('#lname')
+let description = document.querySelector('description')
 
+//Global insert
+fname.style.color = '#363636'
 
-let nome = document.querySelector('#nome')
+//Primeiro nome
+fname.style.width = '100%'
+fname.style.height = '5%'
 
-function validarNome(){
-    let txtNome = document.querySelector('#txtNome')
-    if(nome.value.length < 3){
-       txtNome.innerHTML = 'nome invalido'
-       txtNome.style.color = 'red'
+//Ultimo nome nome
+lname.style.width = '100%'
+lname.style.height = '5%'
+
+//Ultimo nome nome
+description.style.width = '100%'
+description.style.height = '5%'
+
+//Function insert
+function fnameValidation(){
+    let textFname = document.querySelector('#textFname')
+    if(fname.value.length < 3){
+        textFname.innerHTML = 'Inválido ❌' 
+        textFname.style.color = 'red'
     }else{
-        txtNome.innerHTML = 'nome valido'
+        textFname.innerHTML = 'Válido ✅'
+        textFname.style.color = 'green'
+    }
+}
+
+function lnameValidation(){
+    let textLname = document.querySelector('#textLname')
+    if(lname.value.length < 3){
+        textLname.innerHTML = 'Inválido ❌'
+        textLname.style.color = 'red'
+    }else{
+        textLname.innerHTML = 'Válido ✅'
+        textLname.style.color = 'green'
+    }
+}
+
+function descriptionValidation(){
+    let texDescription = document.querySelector('#texDescription')
+    if(description.value.length < 3){
+        texDescription.innerHTML = 'Inválido ❌'
+        texDescription.style.color = 'red'
+    }else{
+        texDescription.innerHTML = 'Válido ✅'
+        texDescription.style.color = 'green'
     }
 }
